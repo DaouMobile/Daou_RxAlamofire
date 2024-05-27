a7ff531fa113b58527a0b95f98073bb88575bb1f
vongvoro <vongvoro@daou.co.kr>
2024-05-28 08:51:13 +0900

    chore: Set default release version at pod_install shell script

--- Examples/pod_install.sh
+++ Examples/pod_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -eo pipefail
-export RELEASE_VERSION="$(git describe --abbrev=0 | tr -d '\n')"
+export RELEASE_VERSION="$(git describe --abbrev=0 | tr -d '\n' || echo 'v6.1.1')"
 RELEASE_VERSION=${RELEASE_VERSION:1}
 xcodegen
 pod install --repo-update
