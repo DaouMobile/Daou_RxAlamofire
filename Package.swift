3f3b79325e47c98d03b303abbf433eb521df473c
vongvoro <vongvoro@daou.co.kr>
2024-07-01 15:19:04 +0900

    chore: change deployment target

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(name: "RxAlamofire",
                       platforms: [
-                        .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v4)
+                        .macOS(.v13), .iOS(.v13), .tvOS(.v12), .watchOS(.v4)
                       ],
                       products: [
                         // Products define the executables and libraries produced by a package, and make them visible to other packages.
